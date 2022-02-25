e378643e292b55d10689feb66f6458d44b7ee8ad
JanaGeo <janii.mayer@gmail.com>
2022-02-25 01:39:58 +0000

    new

---
+++ src/main/java/com/introjava_jana/mod_minecraft/mod_minecraft.java
@@ -0,0 +1,49 @@
+package com.introjava_jana.mod_minecraft;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+import static net.minecraftforge.versions.forge.ForgeVersion.MOD_ID;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(mod_minecraft.MOD_ID)
+public class mod_minecraft
+{
+
+    public static final String MOD_ID = "mod_minecraft";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public mod_minecraft() {
+
+        IEventBus EventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        EventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+//comment
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+
+}
